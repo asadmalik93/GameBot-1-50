7a7571268350f49109918a72c1b0379f63396a8c
asadmalik93 <30842812+asadmalik93@users.noreply.github.com>
2017-08-10 13:45:27 +0500

    Update Controller.java

--- src/Controller.java
+++ src/Controller.java
@@ -9,7 +9,7 @@ public class Controller {
 
 	public static void main(String[] args) {
 
-		System.setProperty("webdriver.chrome.driver", "/Users/asadmalik/Desktop/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "lib"+File.seperator+"chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://zzzscore.com/1to50/en/");
 		
