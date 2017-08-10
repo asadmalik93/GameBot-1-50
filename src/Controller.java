4a4dfcf2c13abf7e13a9a04262aed484d2407353
asadmalik93 <30842812+asadmalik93@users.noreply.github.com>
2017-08-10 13:42:46 +0500

    A game bot that plays the game : http://zzzscore.com/1to50/en/
    Program that utilizes seleneum web driver to play the game listed. The algorithm sorts the buttons into a list and clicks them when in a for loop. The time complexity of the algorithm is O(n)

---
+++ src/Controller.java
@@ -0,0 +1,38 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Controller {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "/Users/asadmalik/Desktop/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://zzzscore.com/1to50/en/");
+		
+		//System.out.println(driver.getPageSource());
+		
+		WebElement[] element = new WebElement[51];
+		
+		List<WebElement> divE = driver.findElement(By.id("grid")).findElements(By.cssSelector("div"));
+		
+		for (int i = 0; i < divE.size(); i++) {
+			element[Integer.parseInt(divE.get(i).getText())] = divE.get(i);
+		}
+		   
+		for (int i = 1; i < element.length; i++) {
+			if (element[i] != null) {
+				element[i].click();
+			} else {
+				divE = driver.findElement(By.id("grid")).findElements(By.cssSelector("div"));
+				for (int y = 0; y < divE.size(); y++) {
+					element[Integer.parseInt(divE.get(y).getText())] = divE.get(y);
+				}
+				element[i].click();
+			}
+		}
+	}
+}
